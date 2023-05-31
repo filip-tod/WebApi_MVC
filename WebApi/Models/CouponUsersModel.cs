8b06e83bc63330d15a4dbac0aa1ff385254f0657
DESKTOP-JN8TE8V\Bracurda <filip.tod95@gmail.com>
2023-05-31 13:57:57 +0200

    napravio contorllere sa joinom "coupon" i "League" fali mi još jedan sa getById();

---
+++ WebApi/Models/CouponUsersModel.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace WebApi.Models
+{
+    public class CouponUsersModel
+    {
+        public int PriceDiscount { get; set; }
+        public bool CouponValidation { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public string NameUser { get; set; }
+        public string LastNameUser { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ WebApi/Models/LeagueTeamModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models
+{
+    public class LeagueTeamModel
+    {
+        public int Id { get; set; }
+        public string Division { get; set;}
+        public string Commissioner { get; set; }
+        public string Teamname { get; set; }
+        
+    }
+}
